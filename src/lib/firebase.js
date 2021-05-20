0b1905a1c9a1303d778ceca518c0fade2804c202
quangnm2405 <quangnm.vnist@gmail.com>
2021-05-20 23:28:47 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyA8715oy4MGfLsc-IEXdGVuml16pzydgQc",
+    authDomain: "itss-pj.firebaseapp.com",
+    projectId: "itss-pj",
+    storageBucket: "itss-pj.appspot.com",
+    messagingSenderId: "440333315638",
+    appId: "1:440333315638:web:f365b60aefd98e9d487297"
+  };
+
+  firebase.initializeApp(firebaseConfig);
