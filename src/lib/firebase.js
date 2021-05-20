c3c946b8b3fb84d7ea6fd896bd33c197b24bb005
quangnm2405 <quangnm.vnist@gmail.com>
2021-05-21 00:35:50 +0700

    Login by Authentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -11,7 +11,8 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
-
+export const auth = firebase.auth();
+export default firebase;
 export const getFirebaseItems = async () => {
   try {
     const data = await db.collection("todos").get();
@@ -46,3 +47,38 @@ export const clearFirebaseItem = async (item) => {
   }).catch(function (err) {
   });
 };
+export const uiConfig = {
+    signInFlow: 'popup',
+    signInSuccessUrl: "/",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+  }
+  
+  export const storeUserInfo = async (user) => {
+    const { uid } = user;
+    const userDoc = await db.collection("users").doc(uid).get();
+    if (!userDoc.exists) {
+      await db.collection("users").doc(uid).set({ name: user.displayName });
+      return {
+        name: user.displayName,
+        id: uid,
+      };
+    } else {
+      return {
+        id: uid,
+        ...userDoc.data(),
+      };
+    }
+  }
+  
+  export const updateUser = async (user, image) => {
+    try {
+      const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+      if (userDoc.exists) {
+        await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
