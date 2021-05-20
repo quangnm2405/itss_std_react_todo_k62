334a97d507f08baa841b9a3facd315f6e98f12ac
quangnm2405 <quangnm.vnist@gmail.com>
2021-05-21 00:44:59 +0700

    Save Avatar by Storage

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -82,3 +82,15 @@ export const uiConfig = {
       console.log(err);
     }
   }
+
+  export const uploadImage = async (image) => {
+    const ref = firebase.storage().ref().child(`/images/${image.name}`);
+    let downloadUrl = "";
+    try {
+      await ref.put(image);
+      downloadUrl = await ref.getDownloadURL();
+    } catch (err) {
+      console.log(err);
+    }
+    return downloadUrl;
+  };
